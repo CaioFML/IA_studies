4ee49b458504acd5d7e01fd742800e81c481456e
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2025-02-04 17:40:59 -0300

    22. Lab: Regressão Linear com StatsModels

---
+++ formacao_completa_inteligencia_artificial_e_machine_learning/section_3/regression_with_stats_models.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as sm
+import scipy.stats as stats
+import seaborn as sns
+
+base = pd.read_csv('mt_cars.csv')
+
+print(base.shape)
+
+print(base.head())
+
+base = base.drop(['Unnamed: 0'], axis = 1)
+
+print(base.head())
+
+corr = base.corr()
+
+print(corr)
+
+sns.heatmap(corr, cmap='coolwarm', annot=True, fmt='.2f')
+
+# plt.show()
+
+column_pairs = [('mpg', 'cyl'), ('mpg', 'disp'), ('mpg', 'hp'), ('mpg', 'wt'), ('mpg', 'qsec'), ('mpg', 'vs'), ('mpg', 'am'), ('mpg', 'gear'), ('mpg', 'carb')]
+
+n_plots = len(column_pairs)
+fig, axes = plt.subplots(nrows=n_plots, ncols=1, figsize=(6,4 * n_plots))
+
+for i, pair in enumerate(column_pairs):
+  x_col, y_col = pair
+  sns.scatterplot(x=x_col, y=y_col, data=base, ax=axes[i])
+  axes[i].set_title(f'{x_col} vs {y_col}')
+
+plt.tight_layout()
+plt.show()
